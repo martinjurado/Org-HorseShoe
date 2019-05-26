492be7bfffb559cf1464d6f5b5c530a3ae1fde54
unknown <juradomartinbenedict@gmail.com>
2019-05-26 09:37:39 -0400

    added redux react-redux, updated App.js, updated client, added store for redux, added Productpage, added cart.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Cart from "./components/shopify/Cart";
+import ProductPage from './components/ProductPage';
+import { connect } from "react-redux";
+import store from "./store";
 
-function App() {
-  return (
-    <div className="App">
-      Hello World
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.updateQuantityInCart = this.updateQuantityInCart.bind(this);
+    this.removeLineItemInCart = this.removeLineItemInCart.bind(this);
+    this.handleCartClose = this.handleCartClose.bind(this);
+    this.handleCartOpen = this.handleCartOpen.bind(this);
+  }
+
+  updateQuantityInCart(lineItemId, quantity) {
+    const state = store.getState(); // state from redux store
+    const checkoutId = state.checkout.id;
+    const lineItemsToUpdate = [
+      { id: lineItemId, quantity: parseInt(quantity, 10) }
+    ];
+    state.client.checkout
+      .updateLineItems(checkoutId, lineItemsToUpdate)
+      .then(res => {
+        store.dispatch({
+          type: "UPDATE_QUANTITY_IN_CART",
+          payload: { checkout: res }
+        });
+      });
+  }
+  removeLineItemInCart(lineItemId) {
+    const state = store.getState(); // state from redux store
+    const checkoutId = state.checkout.id;
+    state.client.checkout
+      .removeLineItems(checkoutId, [lineItemId])
+      .then(res => {
+        store.dispatch({
+          type: "REMOVE_LINE_ITEM_IN_CART",
+          payload: { checkout: res }
+        });
+      });
+  }
+  handleCartClose() {
+    store.dispatch({ type: "CLOSE_CART" });
+  }
+  handleCartOpen() {
+    store.dispatch({type: 'OPEN_CART'});
+  }
+  render() {
+    const state = store.getState();
+    return (
+      <div>
+        <Cart
+          checkout={state.checkout}
+          isCartOpen={state.isCartOpen}
+          handleCartClose={this.handleCartClose}
+          updateQuantityInCart={this.updateQuantityInCart}
+          removeLineItemInCart={this.removeLineItemInCart}
+        />
+        <ProductPage/>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default connect(state => state)(App);
--- src/index.js
+++ src/index.js
@@ -1,16 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import Client from 'shopify-buy';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import Client from "shopify-buy";
+import { Provider } from "react-redux";
+import store from "./store";
+
 
 const client = Client.buildClient({
-    storefrontAccessToken: 'your-access-token',
-    domain: 'your-shopify-url.myshopify.com'
+  storefrontAccessToken: "your-access-token",
+  domain: "your-shopify-url.myshopify.com"
 });
 
-ReactDOM.render(<App />, document.getElementById('root'));
+store.dispatch({ type: "CLIENT_CREATED", payload: client });
+
+// buildClient() is synchronous, so we can call all these after!
+client.product.fetchAll().then(res => {
+  store.dispatch({ type: "PRODUCTS_FOUND", payload: res });
+});
+client.checkout.create().then(res => {
+  store.dispatch({ type: "CHECKOUT_FOUND", payload: res });
+});
+client.shop.fetchInfo().then(res => {
+  store.dispatch({ type: "SHOP_FOUND", payload: res });
+});
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,  
+  document.getElementById('root')
+);
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
